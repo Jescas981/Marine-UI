a44f49b7eca6ec95184f60774690bef2b1377aa0
unknown <Administrador@UTP362399.grupoutp.net>
2024-05-17 20:13:29 -0500

    Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import Canvas from "./Canvas";
 
 const App: React.FC<React.PropsWithChildren> = () => {
-  return <div className="text-white">
+  return <div className="">
+    <h1 className="font-bold text-4xl text-center py-8">Navegacion Asistida</h1>
     <Canvas>
-
     </Canvas>
+    <div className="mx-auto rounded-md shadow-md bg-gray-100 p-4 w-72">
+        <h1 className="font-bold">Coordinates</h1>
+        <p>Orientation: 45.123° West</p>
+        <p>Longitude: -75.015152</p>
+        <p>Latitude: -9.189967</p>
+        <p>Velocity: 10.0512 m/s</p>
+    </div>
   </div>;
 };
 
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -3,20 +3,21 @@ import React, { useEffect, useRef, useState } from "react";
 const Canvas: React.FC<React.PropsWithChildren> = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [counter, setCounter] = useState(0);
+  const [counter100, setCounter100] = useState(0);
 
   useEffect(() => {
     // Set interval
     const interval = setInterval(() => {
       setCounter(counter + 1);
-      console.log(counter);
     }, 1000);
+
     // Set canvas
     if (canvasRef.current == null) return;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     if (ctx == null) return;
 
-    const center = { x: 400, y: 400 };
+    const center = { x: 300, y: 300 };
     const radius = 200;
     const circles = 6;
     const open = Math.PI * 0.05;
@@ -35,7 +36,13 @@ const Canvas: React.FC<React.PropsWithChildren> = () => {
 
     for (let i = circles; i >= 0; i--) {
       ctx.beginPath();
-      ctx.arc(center.x, center.y, radius * i * step, rand - Math.PI + open, rand+Math.PI - open);
+      ctx.arc(
+        center.x,
+        center.y,
+        radius * i * step,
+        rand - Math.PI + open,
+        rand + Math.PI - open
+      );
       ctx.strokeStyle = "white";
       ctx.lineWidth = 5;
       ctx.stroke();
@@ -43,21 +50,59 @@ const Canvas: React.FC<React.PropsWithChildren> = () => {
       ctx.closePath();
     }
 
-    for (let i = 0; i <180; i++) {
-        ctx.beginPath();
-        ctx.moveTo(0,0)
-        ctx.lineTo(400,400)
-        ctx.stroke()
-        ctx.closePath();
-      }
-    
+    for (let i = 0; i < 180; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(300, 300);
+      ctx.stroke();
+      ctx.closePath();
+    }
+    ctx.beginPath();
+    const rand2 = (Math.random() * 2 -1) * 50;
+    const rand3 = (Math.random() * 2 -1) * 50;
+    ctx.arc(center.x + 100 + rand2, center.y + rand3 + 100, 8, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 1;
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
 
     return () => {
       clearInterval(interval);
     };
   }, [counter]);
-  return <canvas ref={canvasRef} height={800} width={800}></canvas>;
+
+
+//   useEffect(()=>{
+//     const interval_100 = setInterval(() => {
+//       setCounter100(counter100 + 1);
+//     }, 200);
+
+//         // Set canvas
+//         if (canvasRef.current == null) return;
+//         const canvas = canvasRef.current;
+//         const ctx = canvas.getContext("2d");
+//         if (ctx == null) return;
+//         const center = { x: 300, y: 300 };
+
+
+   
+
+//     return () => {
+//       clearInterval(interval_100);
+//   }
+// },[counter100]);
+
+  return (
+    <canvas
+      className="mx-auto"
+      ref={canvasRef}
+      height={600}
+      width={600}
+    ></canvas>
+  );
 };
 
 export default Canvas;
